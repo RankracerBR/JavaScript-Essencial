81282b9a70ab43941a456fa2381414befcec4545
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-01-15 08:25:19 -0300

    Create variaveis4.js

---
+++ variaveis4.js
@@ -0,0 +1,12 @@
+(() => {
+    const test = 'valor função';
+    console.log(`Valor dentro da função "${test}"`);
+
+    if (true) {
+        const test = 'valor if';
+        console.log(`Valor dentro do if "${test}"`)
+    }
+    
+    console.log(`Valor após a execução do if "${test}"`);
+
+})();
