f347a9c42c7d9e574d873e5262018a2810c377f1
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-01-15 08:25:14 -0300

    Create variaveis2.js

---
+++ variaveis2.js
@@ -0,0 +1,11 @@
+var test = 'Example';
+
+(() => {
+console.log(`Valor dentro da função "${test}"`);
+if (true) {
+    var test
+    console.log(`Valor dentro do if "${test}"`);
+}
+console.log(`Valor após a execução do if "${test}"`);
+
+})();
