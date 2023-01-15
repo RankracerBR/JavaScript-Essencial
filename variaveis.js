e21b29991ab02810d4c784e85a57b3d716460314
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-01-15 08:25:12 -0300

    Create variaveis.js

---
+++ variaveis.js
@@ -0,0 +1,7 @@
+var nameVar = 'Augusto';
+let nameLet = 'Augusto';
+const nameConst = 'Augusto';
+
+console.log(`nameVar: ${nameVar}`);
+console.log(`nameLet: ${nameLet}`);
+console.log(`nameConst: ${nameConst}`);
