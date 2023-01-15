44a0d88804bb0e24422f15d0306a484bad644de5
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-01-15 08:25:03 -0300

    Create Hoisting.js

---
+++ Hoisting.js
@@ -0,0 +1,17 @@
+function fn(){
+    console.log(text);
+    var text = 'Exempo';
+    
+    console.log(text)
+}
+
+fn();
+/*
+function fn(){
+    var text;
+    console.log(text);
+
+    text = 'Exemplo';
+
+    console.log(text);
+} */
