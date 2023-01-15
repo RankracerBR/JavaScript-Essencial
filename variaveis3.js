8e39ae8e4e3e5e48d718119512f33c5fae476322
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-01-15 08:25:17 -0300

    Create variaveis3.js

---
+++ variaveis3.js
@@ -0,0 +1,12 @@
+(() => {
+    let test = 'valor função';
+    console.log(`Valor dentro da função "${test}"`);
+
+    if (true) {
+        let test = 'valor if';
+        console.log(`Valor dentro do if "${test}"`);
+    }
+
+    console.log(`Valor após a execução do if "${test}"`);
+
+})();
