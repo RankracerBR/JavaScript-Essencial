d609f544a2e4580b7881293157263b202dda28e7
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-01-21 09:15:12 -0300

    Create Comparacao.js

---
+++ Comparacao.js
@@ -0,0 +1,38 @@
+// Igual (==)
+//Retorna verdadeiro caso os operandos sejam iguais 
+"3" == var1 
+3 == '3'
+
+//Não igual(!=)
+//Retorna verdadeiros caso os operandos não sejam iguais 
+var2 != "3"
+
+//Estritamente igual (===)
+//Retorna verdadeiro caso os operandos sejam iguais e do mesmo tipo
+3 === var1
+
+//Estritamente não igual (!==)
+//Retorna verdadeiro caso os operandos não sejam iguais e/ou não sejam do mesmo tipo
+var1 !== "3"
+3 !== "3"
+
+//Maior que (>)
+//Retorna verdadeiro caso o operando da esquerda seja maior que o da direita
+var2 > var1 
+"12" > 2
+
+//Maior que ou igual (>=)
+//Retorna verdadeiro caso o operando da esquerda seja maior ou igual ao da direita
+var2 >= var1 
+var1 >= 3
+
+//Menor que (<)
+//Retorna verdadeiro caso o operando da esquerda seja maior ou igual ao da direita
+var2 < var1
+"12" < "2"
+
+//Menor ou igual (<=)
+//Retorna verdadeiro caso o operando da esquerda seja menor ou igual ao da direita
+
+var1 <= var2
+var2 <= 5 
