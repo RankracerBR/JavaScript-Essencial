5e67f6c9cc3bb2d232f932d77edd7dea879f8c94
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-01-21 09:14:17 -0300

    Create Aritmeticos.js

---
+++ Aritmeticos.js
@@ -0,0 +1,28 @@
+//Modulo (%)
+// Operador binário. Retorna o inteiro restante da divisão dos dois operandos
+
+12 % 5 // retorna 2
+
+//Incremento (++) e Decremento (--)
+++x 
+x++
+
+--x 
+x--
+
+const a = ++2; // 3
+const b = 2++; // 2
+
+//Negação (-) e Adição (+)
+-3 
++"3" //retorna 3
++true //retorna 1
++false //retorna 0
+-true //retorna -1
+
+//Operador de exponenciação (**)
+2 ** 3 //retorna 0
+10 ** -1 //retorna 0.1
+
+//Operador de agrupamento ()
+2 * (3+2)
