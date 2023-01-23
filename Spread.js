494802a42b509b5555e1669227cee30a95512751
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2023-01-23 03:38:45 -0300

    Create Spread.js

---
+++ Spread.js
@@ -0,0 +1,7 @@
+//Spread 
+var partes = ['ombro','joelhos'];
+var musica = ['cabeca',...partes, 'e','pés']; //['cabeca','ombro','joelhos','e','pés']
+
+function fn(x,y,z) { }
+var args = [0,1,2];
+fn(...args);
